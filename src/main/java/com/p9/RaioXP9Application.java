61ef5c727bd187e17635f23bd5f60f94cb249bf8
Matheus Henrique <matheus.henrique@ifood.com.br>
2022-01-27 15:39:31 -0300

    first-controller

--- src/main/java/com/p9/RaioXP9Application.java
+++ src/main/java/com/p9/RaioXP9Application.java
@@ -2,9 +2,19 @@ package com.p9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@RestController
 @SpringBootApplication
 public class RaioXP9Application {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaioXP9Application.class, args);
